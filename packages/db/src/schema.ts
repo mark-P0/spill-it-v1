4f39157840929f2a4d0895c3fae860c5014c66df
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-23 00:49:08 +0800

    feat(db,schema): add email and password columns to users table

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,8 +1,12 @@
-import { pgTable, serial, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const UsersTable = pgTable("users", {
   id: serial("id").primaryKey(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at"),
   deletedAt: timestamp("deleted_at"),
+  email: text("email").notNull(),
+  password: text("password").notNull(),
 });
+
+export type User = typeof UsersTable.$inferSelect;
