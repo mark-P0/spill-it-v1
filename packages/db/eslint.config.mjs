9b7619383453f47d8c1cf9e6f32bf12a87734c00
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-23 01:55:03 +0800

    build(db,eslint): install eslint

---
+++ packages/db/eslint.config.mjs
@@ -0,0 +1,20 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+const configs = [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    /** https://typescript-eslint.io/rules/no-unused-vars */
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+];
+
+export default configs;
