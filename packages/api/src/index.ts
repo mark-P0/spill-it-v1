91ef61c1ccd2b690714f5b49c1e81e14ac861f88
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-23 01:18:19 +0800

    feat(api,context): add db functions to context

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,5 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import "@spill-it-v1/db";
 import { env } from "./config/env";
 import { context } from "./context";
 import { resolvers } from "./resolvers";
