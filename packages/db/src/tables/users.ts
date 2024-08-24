51814f3cd59bc1083409a15a83a5e9b9b4446e9b
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-24 13:03:48 +0800

    feat(db.users): create function for creating users

--- packages/db/src/tables/users.ts
+++ packages/db/src/tables/users.ts
@@ -1,4 +1,5 @@
 import { db } from "../db";
+import { UsersTable } from "../schema";
 
 export async function getUserById(id: number) {
   const users = await db.query.UsersTable.findMany({
@@ -11,4 +12,21 @@ export async function getUserById(id: number) {
   const user = users[0] ?? null;
 
   return user;
+}
+
+export async function createUser(email: string, password: string) {
+  const newUsers = await db
+    .insert(UsersTable)
+    .values({ email, password })
+    .returning();
+  if (newUsers.length > 1) {
+    throw new Error("Multiple users created...?");
+  }
+
+  const newUser = newUsers[0]
+  if (newUser === undefined) {
+    throw new Error('Created user does not exist...?')
+  }
+
+  return newUser
 }
