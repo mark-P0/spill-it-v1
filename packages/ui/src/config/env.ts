d593a142d94209a2b2f86d30bd63c332ee769648
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-26 01:01:56 +0800

    feat(ui.google): add google client id to env config

--- packages/ui/src/config/env.ts
+++ packages/ui/src/config/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   VITE_GRAPHQL_SERVER_URL: z.string(),
+  VITE_GOOGLE_CLIENT_ID: z.string(),
 });
 
 export const env = envSchema.parse(import.meta.env);
