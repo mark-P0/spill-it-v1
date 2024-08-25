eaaa926737725130324da7c2c0a7fd6d46551dc6
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-08-25 17:15:33 +0800

    refactor(gql): rename definitions document

--- packages/gql/codegen.ts
+++ packages/gql/codegen.ts
@@ -1,14 +1,14 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "./documents.graphql",
+  schema: "./definitions.graphql",
   ignoreNoDocuments: true,
 
   generates: {
     /** https://the-guild.dev/graphql/codegen/docs/guides/vanilla-typescript */
     "./codegen/ui/": {
       preset: "client",
-      documents: "./documents.graphql",
+      documents: "./definitions.graphql",
       config: {
         documentMode: "string",
       },
