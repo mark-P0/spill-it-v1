fec5493b2ab9bcedf359e84226691665aa51e6a6
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-01 02:19:48 +0800

    feat(api.env): get token signing key from env

--- packages/api/src/config/env.ts
+++ packages/api/src/config/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.union([z.literal("development"), z.literal("production")]),
   PORT: z.coerce.number(),
+  TOKEN_KEY: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
