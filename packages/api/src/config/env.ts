6002cbb4dbf5a965d2db49d3bfd167a60ed7e790
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-01 19:19:49 +0800

    refactor(api.env): simplify node env union into enum

--- packages/api/src/config/env.ts
+++ packages/api/src/config/env.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.union([z.literal("development"), z.literal("production")]),
+  NODE_ENV: z.enum(["development", "production"]),
   PORT: z.coerce.number(),
   TOKEN_KEY: z.string(),
 });
-
 export const env = envSchema.parse(process.env);
