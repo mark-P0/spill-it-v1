e987f5ed45a44c0f6c13c7310112ccf92b30614c
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-01 21:31:29 +0800

    feat(ui.query): use react query client provider

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,9 +1,10 @@
+import { QueryProvider } from "./features/react-query";
 import { HomeScreen } from "./features/react/HomeScreen";
 
 export function App() {
   return (
-    <>
+    <QueryProvider>
       <HomeScreen />
-    </>
+    </QueryProvider>
   );
 }
