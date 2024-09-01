b2e3a11209547f647066ab32dc6d05742c0a6f8a
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-01 21:38:41 +0800

    feat(ui): exchange google token for access token

--- packages/ui/src/SignInWithGoogleButton.tsx
+++ packages/ui/src/SignInWithGoogleButton.tsx
@@ -1,5 +1,21 @@
+import { GetAccessTokenDocument } from "@spill-it-v1/gql/codegen/ui/graphql";
 import { useEffect, useRef } from "react";
 import { env } from "./config/env";
+import { gqlFetch } from "./utils/gql-fetch";
+
+async function exchangeGoogleTokenForAccessToken(googleToken: string) {
+  const res = await gqlFetch({
+    document: GetAccessTokenDocument,
+    variables: { input: { googleToken } },
+  });
+
+  const accessToken = res.data?.accessToken;
+  if (accessToken === undefined) {
+    throw new Error("Failed exchanging Google token for access token");
+  }
+
+  return accessToken;
+}
 
 /**
  * - https://developers.google.com/identity/gsi/web/guides/display-button#button_rendering
@@ -14,10 +30,12 @@ export function SignInWithGoogleButton() {
 
     google.accounts.id.initialize({
       client_id: env.VITE_GOOGLE_CLIENT_ID,
-      callback(res) {
+      async callback(res) {
         const { credential } = res;
 
-        console.warn({ credential });
+        const accessToken = await exchangeGoogleTokenForAccessToken(credential);
+
+        console.warn({ credential, accessToken });
       },
     });
 
