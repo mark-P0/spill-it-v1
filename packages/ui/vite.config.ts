7d0ec0b4f1c7f0da90433d9821ce39e1bd7aa217
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-08 00:07:11 +0800

    refactor(ui.vite): specify dev port on vite config

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { z } from "zod";
+
+const zEnv = z.object({
+  UI_PORT: z.coerce.number(),
+});
+const env = zEnv.parse(process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: env.UI_PORT },
 });
