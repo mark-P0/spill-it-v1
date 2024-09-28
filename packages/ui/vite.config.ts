c58bc6e05554951ed07e4e476f61fc79a3a03ca6
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-09-28 14:50:35 +0800

    build(ui.vite): load env files on dev mode

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,14 +1,23 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { z } from "zod";
 
 const zEnv = z.object({
   UI_PORT: z.coerce.number(),
 });
-const env = zEnv.parse(process.env);
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: env.UI_PORT },
+/**
+ * - https://vitejs.dev/config/
+ * - https://vitejs.dev/config/#using-environment-variables-in-config
+ */
+const config = defineConfig(({ mode }) => {
+  const envRaw = loadEnv(mode, process.cwd(), "");
+  const env = zEnv.parse(envRaw);
+
+  return {
+    plugins: [react()],
+    server: { port: env.UI_PORT },
+  };
 });
+
+export default config;
