05453e8cc03799dcea28bb6c4778707008ab6d9f
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-10-17 22:09:06 +0800

    feat(ui.own-user): automatically log out on query fail

--- packages/ui/src/features/queries/own-user.ts
+++ packages/ui/src/features/queries/own-user.ts
@@ -1,15 +1,23 @@
 import { GetOwnUserDocument } from "@spill-it-v1/gql/codegen/ui/graphql";
 import { skipToken, useQuery } from "@tanstack/react-query";
-import { useAccessTokenQuery } from "../../features/queries/access-tokens";
+import {
+  useAccessTokenMutation,
+  useAccessTokenQuery,
+} from "../../features/queries/access-tokens";
 import { gqlFetch } from "../graphql/gql-fetch";
 
 export function useOwnUserQuery() {
   const { data: accessToken } = useAccessTokenQuery();
+  const accessTokenMutation = useAccessTokenMutation();
 
   const hasAccessToken = accessToken !== undefined && accessToken !== null;
   const canQueryOwnUser = hasAccessToken;
   const ownUserQuery = useQuery({
     queryKey: ["own-user", accessToken],
+
+    /** Retry with quick fixed interval */
+    ...{ retry: 2, retryDelay: 500 },
+
     queryFn: !canQueryOwnUser
       ? skipToken
       : () =>
@@ -18,6 +26,13 @@ export function useOwnUserQuery() {
             variables: { accessToken },
           }),
   });
+
+  const isUserNotAvailable =
+    ownUserQuery.status === "error" && ownUserQuery.fetchStatus !== "fetching";
+  const isLogoutOngoing = accessTokenMutation.status === "pending";
+  if (isUserNotAvailable && !isLogoutOngoing) {
+    accessTokenMutation.mutate({ action: "remove" });
+  }
 
   return ownUserQuery;
 }
